8178979f73e4ddbf6d3b926a9646248d0011ba2e
joel joseph <joeljordanjoseph@gmail.com>
2022-01-16 15:47:32 -0700

    about page text added

--- src/pages/About-Us/About-Us.js
+++ src/pages/About-Us/About-Us.js
@@ -10,7 +10,11 @@ class AboutUs extends React.Component{
                 </div>
                 <div className='statement'>
                     <p className='statement-body'>
-                        Stutor is a platform where 
+                        Stutor is an industry leading on-demand tutoring platform connecting students-to-students. Using AI to match you to the perfect tutor, Stutor matches students to other students at your university so they can get the class specific help they need, whenever they need. 
+                        <br></br>
+                        <br></br>
+                        As a student, you get the help you need, as a stutor, you make a living wage in your spare time. It’s a true win-win. 
+                        Stay tuned as Stutor comes to your university! 
                     </p>
                 </div>
             </div>
